6e291c8f31de592f6832b1e6624d3e20614fc79d
George <69978112+gobelluncg@users.noreply.github.com>
2020-11-25 16:17:43 -0500

    Add files via upload

--- NCAAFModel.java
+++ NCAAFModel.java
@@ -1,84 +1,82 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pocketscoresapitranslator;
 
 
 public class NCAAFModel {
     
-    sportsFeedTranslator SFAPITranslator = new sportsFeedTranslator("NCAAF", "");
+    protected SportsFeedTranslator SFAPITranslator = new SportsFeedTranslator("NCAAF", "");
     
     public NCAAFModel(String date){
-        this.SFAPITranslator = new sportsFeedTranslator("NCAAF", date);
+        this.SFAPITranslator = new SportsFeedTranslator("NCAAF", date);
     }
+    
+    //=================================GETTERS=================================
     
     public int getNumGames(){
         return this.SFAPITranslator.getNumOfGames();
     }
     
-    public String getStartTime(int gameNum){
-        return this.SFAPITranslator.getStartTime(gameNum);
+    public String getStartTime(int _gameNum){
+        return this.SFAPITranslator.getStartTime(_gameNum);
     }
     
-    public String getStatus(int gameNum){
-        return this.SFAPITranslator.getStatus(gameNum);
+    public String getStatus(int _gameNum){
+        return this.SFAPITranslator.getStatus(_gameNum);
     }
     
-    public String getCurrentQuarter(int gameNum){
-        return this.SFAPITranslator.getCurrentPeriod(gameNum);
+    public String getCurrentQuarter(int _gameNum){
+        return this.SFAPITranslator.getCurrentPeriod(_gameNum);
     }
     
-    public String getRemainingTime(int gameNum){
-        return this.SFAPITranslator.getRemainingTime(gameNum);
+    public String getRemainingTime(int _gameNum){
+        return this.SFAPITranslator.getRemainingTime(_gameNum);
     }
     
-    public String getHomeTeam(int gameNum){
-        return this.SFAPITranslator.getHomeTeamName(gameNum);
+    public String getHomeTeam(int _gameNum){
+        return this.SFAPITranslator.getHomeTeamName(_gameNum);
     }
     
-    public String getAwayTeam(int gameNum){
-        return this.SFAPITranslator.getAwayTeamName(gameNum);
+    public String getAwayTeam(int _gameNum){
+        return this.SFAPITranslator.getAwayTeamName(_gameNum);
     }
     
-    public String getHomeScore(int gameNum){
-        return this.SFAPITranslator.getHomeScore(gameNum);
+    public String getHomeScore(int _gameNum){
+        return this.SFAPITranslator.getHomeScore(_gameNum);
     }
     
-    public String getAwayScore(int gameNum){
-        return this.SFAPITranslator.getAwayScore(gameNum);
+    public String getAwayScore(int _gameNum){
+        return this.SFAPITranslator.getAwayScore(_gameNum);
     }
     
-    public String getFirstHomeQuarter(int gameNum){
-        return this.SFAPITranslator.getFirstQuarterHome(gameNum);
+    public String getFirstHomeQuarter(int _gameNum){
+        return this.SFAPITranslator.getFirstQuarterHome(_gameNum);
     }
     
-    public String getSecondHomeQuarter(int gameNum){
-        return this.SFAPITranslator.getSecondQuarterHome(gameNum);
+    public String getSecondHomeQuarter(int _gameNum){
+        return this.SFAPITranslator.getSecondQuarterHome(_gameNum);
     }
     
-    public String getThirdHomeQuarter(int gameNum){
-        return this.SFAPITranslator.getThirdQuarterHome(gameNum);
+    public String getThirdHomeQuarter(int _gameNum){
+        return this.SFAPITranslator.getThirdQuarterHome(_gameNum);
     }
     
-    public String getFourthHomeQuarter(int gameNum){
-        return this.SFAPITranslator.getFourthQuarterHomeFootball(gameNum);
+    public String getFourthHomeQuarter(int _gameNum){
+        return this.SFAPITranslator.getFourthQuarterHomeFootball(_gameNum);
     }
     
-    public String getFirstAwayQuarter(int gameNum){
-        return this.SFAPITranslator.getFirstQuarterAway(gameNum);
+    public String getFirstAwayQuarter(int _gameNum){
+        return this.SFAPITranslator.getFirstQuarterAway(_gameNum);
     }
     
-    public String getSecondAwayQuarter(int gameNum){
-        return this.SFAPITranslator.getSecondQuarterAway(gameNum);
+    public String getSecondAwayQuarter(int _gameNum){
+        return this.SFAPITranslator.getSecondQuarterAway(_gameNum);
     }
     
-    public String getThirdAwayQuarter(int gameNum){
-        return this.SFAPITranslator.getThirdQuarterAway(gameNum);
+    public String getThirdAwayQuarter(int _gameNum){
+        return this.SFAPITranslator.getThirdQuarterAway(_gameNum);
     }
     
-    public String getFourthAwayQuarter(int gameNum){
-        return this.SFAPITranslator.getFourthQuarterAwayFootball(gameNum);
+    public String getFourthAwayQuarter(int _gameNum){
+        return this.SFAPITranslator.getFourthQuarterAwayFootball(_gameNum);
     }
 }
